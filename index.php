5b9814ccecd6611c23d6c555c6d55986286f9e78
Anton Shchukin <cypher76@gmail.com>
2013-11-01 09:57:52 +0700

    Complex of small changes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'functions.php'; ?>
 <!DOCTYPE html>
-<html class="<?php echo getOS();?> <?php echo getBrowser(); ?> no-js" lang="ru">
+<html class="<?php echo getOS();?> <?php echo getBrowser(); ?> no-js" lang="ru" itemscope itemtype="http://schema.org/[%type%]" xmlns:og="http://ogp.me/ns#">
     <head>
 
         <meta charset="utf-8">
@@ -61,9 +61,7 @@
                     <header class="header">
 
                         <div class="header__logo">
-                            <a class="logo" href="#">
-                                <img class="logo__image" src="images/logo.png" width="" height="" alt="" title="">
-                            </a>
+                            <a class="logo" href="#"></a>
                         </div>
 
                     </header>
@@ -79,14 +77,29 @@
                     <ul class="nav">
                         <li class="nav__item">
                             <a class="nav__item__link" href="#"></a>
-                        </li>
-                        <li class="nav__item">
+                        </li><!--
+                     --><li class="nav__item">
                             <a class="nav__item__link" href="#"></a>
-                        </li>
-                        <li class="nav__item">
+                        </li><!--
+                     --><li class="nav__item">
                             <a class="nav__item__link" href="#"></a>
-                        </li>
-                        <li class="nav__item">
+                        </li><!--
+                     --><li class="nav__item">
+                            <a class="nav__item__link" href="#"></a>
+                        </li><!--
+                     --><li class="nav__item">
+                            <a class="nav__item__link" href="#"></a>
+                        </li><!--
+                     --><li class="nav__item">
+                            <a class="nav__item__link" href="#"></a>
+                        </li><!--
+                     --><li class="nav__item">
+                            <a class="nav__item__link" href="#"></a>
+                        </li><!--
+                     --><li class="nav__item">
+                            <a class="nav__item__link" href="#"></a>
+                        </li><!--
+                     --><li class="nav__item">
                             <a class="nav__item__link" href="#"></a>
                         </li>
                     </ul>
@@ -102,11 +115,16 @@
 
                         <div class="body__sidebar">
                             <div class="sidebar">
-
+                                <div class="sidebar__item">
+                                    Sidebar item
+                                </div>
+                                <div class="sidebar__item">
+                                    Sidebar item
+                                </div>
                             </div>
-                        </div>
+                        </div><!--
 
-                        <div class="body__content">
+                    --><div class="body__content">
 
                             <div class="content">
                                 <div class="content__title">
@@ -129,7 +147,8 @@
                                         <input type="submit" class="button" value="input button">
                                         <button class="button">button button</button>
                                         <input class="field" type="text" placeholder="field">
-                                        <!--<input class="field field_large" type="text" placeholder="field_large">-->
+                                        <input class="field field_wide" type="text" placeholder="field_wide">
+                                        <input class="field field_narrow" type="text" placeholder="field_narrow">
                                         <div class="button button_large">button_large</div>
 
                                         <div class="video">
@@ -165,43 +184,24 @@
                                         </table>
 
                                         <h2>H2 header H2</h2>
-                                        <p>Sed id elit augue. Maecenas enim sem, iaculis eget adipiscing eu, dapibus vel magna. Integer in libero nunc, a iaculis est. Suspendisse elementum, mi quis malesuada pretium, ante magna placerat sapien, non euismod nisl libero nec enim. Etiam ut tortor a neque suscipit porta et et nisl. Morbi gravida elit sed risus rhoncus vestibulum. Duis ultrices placerat purus vitae pharetra. Maecenas euismod convallis commodo. Aenean sodales varius dui eget faucibus. Morbi at risus metus, vel varius nibh. Praesent facilisis augue sed erat aliquet congue. Morbi malesuada ornare sollicitudin. Nunc nec est tortor, sed sollicitudin tellus. Aliquam lacinia leo scelerisque turpis vehicula adipiscing. Donec convallis aliquam nibh, eget venenatis dolor dignissim vitae.</p>
-
                                         <h3>H3 header H3</h3>
-                                        <p>Nulla vel ligula vitae magna cursus consequat in eget arcu. Pellentesque ligula justo, malesuada ac sodales vel, mollis nec lorem. Suspendisse semper varius erat nec aliquet. Suspendisse aliquet facilisis mollis. Maecenas felis ipsum, facilisis vitae tempor at, sagittis accumsan est. Suspendisse orci eros, pharetra ac tempor pretium, condimentum ac nulla. Morbi condimentum tincidunt enim at eleifend. Donec consectetur eros ac metus laoreet dignissim. Fusce purus libero, vehicula in sagittis ut, aliquam eget quam. Fusce orci purus, sodales ac accumsan et, volutpat id justo. Nunc urna elit, consectetur sed volutpat nec, elementum vitae justo. Sed fringilla commodo mauris id aliquam. Sed nec justo lacus. Fusce at nunc vitae eros pellentesque ultrices.</p>
-
                                         <h4>H4 header H4</h4>
-                                        <p>Curabitur enim mi, accumsan ac lobortis sit amet, imperdiet id odio. Donec eu ante et eros varius suscipit. Sed fringilla cursus justo, non consequat dolor placerat a. Fusce magna arcu, tincidunt vitae pulvinar in, gravida sed erat. Cras vitae tortor risus. Nam eu nisi odio. Quisque nisl diam, consequat eu congue a, elementum nec risus. Phasellus consectetur bibendum tincidunt. Suspendisse a velit sem. Nam malesuada nunc sed eros pulvinar convallis ullamcorper risus porttitor.</p>
                                         <p><strong>Lorem ipsum dolor sit amet, strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong strong </strong> Donec odio purus, semper nec rhoncus sed, ultricies non sem. Cras ante nisi, imperdiet a pulvinar vitae, dignissim. Pellentesque habitant morbi <a href="#">a href link a href link a href link a href link a href link a href link a href link </a>. Donec odio purus, semper nec rhoncus sed, ultricies non sem. Cras ante nisi, imperdiet a pulvinar vitae, dignissim. <em>em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em em </em>Vivamus sit amet sapien nulla. Nunc orci enim, dictum id faucibus sed, ullamcorper nec mauris. Aenean quam dolor, consequat non rutrum aliquam, viverra vel odio. Fusce volutpat nisl at quam pulvinar et semper quam pharetra. Donec malesuada consectetur ultrices. Aenean sem dui, imperdiet quis euismod a, luctus et libero.</p>
                                         <blockquote>blockquote blockquote blockquote blockquote blockquote blockquote blockquote blockquote blockquote blockquote blockquote blockquote blockquote blockquote Morbi feugiat tellus vel justo pharetra vel hendrerit tortor congue. Vivamus dui leo, porttitor eget ultricies non, tincidunt nec leo. In hac habitasse platea dictumst. Pellentesque mattis sagittis iaculis. Pellentesque lacinia neque in tortor viverra volutpat suscipit tortor placerat. Suspendisse potenti. Nulla purus tortor, hendrerit sit amet tristique ut, lacinia at nibh. Vivamus vehicula lacus id est viverra eget euismod lorem blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam eu lacus diam, et consequat urna. Curabitur neque magna, tempus ac tristique vel, ultrices sollicitudin tellus. Pellentesque nisl mi, ullamcorper eget feugiat vitae, sodales ac tellus. Nulla venenatis dolor in eros elementum a fringilla tellus dictum. Sed lacus tellus, sollicitudin et cursus vel, mollis at est.</blockquote>
                                         <p>Nullam ac lectus ut elit adipiscing lacinia quis nec nisl. Cras lacinia orci a nunc tempus nec consectetur ante mattis. Integer eu diam nulla, nec elementum magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec hendrerit lacinia hendrerit. Vivamus nec sapien eu turpis tincidunt rhoncus sed in quam. Suspendisse potenti. Ut pretium, sapien non feugiat molestie, arcu tellus porttitor purus, vehicula fringilla enim magna sed risus. Donec varius viverra mauris quis porta. Proin auctor gravida consequat. Quisque vel mi quis neque suscipit blandit fringilla ut quam. Aliquam ultricies pharetra velit, sed venenatis nunc pretium eget. Pellentesque urna odio, feugiat vel facilisis at, fermentum at nibh. Suspendisse potenti. Sed ut elit euismod arcu lobortis feugiat in a ligula. Donec faucibus nunc eu orci auctor vulputate.</p>
                                         <hr>
-                                        <p>Sed magna ante, porta varius vulputate non, lobortis eget urna. Nullam aliquet lacus vitae quam tincidunt a lacinia elit mattis. Donec leo ipsum, pulvinar non consequat nec, fringilla et tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec odio augue, condimentum non consequat sit amet, aliquam pharetra tellus. Sed rhoncus porta odio, eu bibendum justo condimentum in. Ut diam tellus, luctus nec auctor ac, posuere ut mauris. Mauris in viverra massa. Morbi est quam, mattis quis pellentesque eget, tempus ullamcorper nisl. Aenean porta aliquet laoreet. Quisque non metus id eros congue tincidunt sed eget mauris. Aliquam id ligula ligula, et tempus ipsum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam dapibus feugiat augue eget scelerisque.</p>
                                         <ul>
                                             <li>Nunc dapibus tristique nibh. Proin ullamcorper tortor id odio porta ornare. Cras euismod nunc non nisi dignissim eu consectetur orci pharetra. Pellentesque sit amet leo ac tellus placerat luctus ac blandit ipsum.</li>
                                             <li>Sed in ipsum nibh, varius molestie sapien. Suspendisse potenti. Nulla lacinia justo at lectus pellentesque non placerat mauris tincidunt. In non convallis nisi. Fusce gravida leo sed nulla imperdiet faucibus.</li>
                                             <li>Sed auctor ullamcorper consequat. Aliquam erat volutpat. Cras nisi ipsum, consequat ut adipiscing quis, dignissim sed eros. In hac habitasse platea dictumst. Donec sit amet leo at elit egestas laoreet at dapibus diam.</li>
                                             <li>Phasellus in tortor sapien, non feugiat urna. Mauris ut pellentesque erat. Donec lacinia adipiscing quam, faucibus aliquam velit hendrerit in. Etiam pulvinar magna sed tortor malesuada et imperdiet justo auctor.</li>
                                         </ul>
-                                        <p>Nunc dapibus tristique nibh. Proin ullamcorper tortor id odio porta ornare. Cras euismod nunc non nisi dignissim eu consectetur orci pharetra. Pellentesque sit amet leo ac tellus placerat luctus ac blandit ipsum. Aliquam semper rhoncus ipsum a consectetur. Nulla facilisi. Aenean fermentum ullamcorper ligula id egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin tincidunt metus congue est aliquam pharetra. Pellentesque ac lorem justo, ut placerat dui. Maecenas ultricies nisi in risus facilisis sed tempus dui lobortis. Donec at est in arcu eleifend ultricies in hendrerit mauris. Maecenas cursus facilisis eros sit amet dapibus. Donec in leo dui. Fusce vitae mauris quis augue tristique ornare.</p>
                                         <ol>
                                             <li>Nunc <q>dapibus tristique nibh</q>. Proin ullamcorper tortor id odio porta ornare. Cras euismod nunc non nisi dignissim eu consectetur orci pharetra. Pellentesque sit amet leo ac tellus placerat luctus ac blandit ipsum.</li>
                                             <li>Sed in ipsum nibh, varius molestie sapien. Suspendisse potenti. Nulla lacinia justo at lectus pellentesque non placerat mauris tincidunt. In non convallis nisi. Fusce gravida leo sed nulla imperdiet faucibus.</li>
                                             <li>Sed auctor ullamcorper consequat. Aliquam erat volutpat. Cras nisi ipsum, consequat ut adipiscing quis, dignissim sed eros. In hac habitasse platea dictumst. Donec sit amet leo at elit egestas laoreet at dapibus diam.</li>
                                             <li>Phasellus in tortor sapien, non feugiat urna. Mauris ut pellentesque erat. Donec lacinia adipiscing quam, faucibus aliquam velit hendrerit in. Etiam pulvinar magna sed tortor malesuada et imperdiet justo auctor.</li>
                                         </ol>
-                                        <p>Ut vel elit risus. Vivamus sit amet sapien nulla. Nunc orci enim, dictum id faucibus sed, ullamcorper nec mauris. Aenean quam dolor, consequat non rutrum aliquam, viverra vel odio. Vestibulum tincidunt egestas elit consectetur pellentesque. Aliquam erat volutpat. Pellentesque nunc neque, pretium non gravida sit amet, scelerisque id purus. Curabitur quam arcu, congue quis volutpat ut, condimentum nec lorem. Donec sit amet aliquet lectus. Morbi sit amet justo velit, non mattis dui. Suspendisse egestas velit ac augue lacinia rutrum.</p>
-                                        <dl>
-                                            <dt>Term term term</dt>
-                                            <dd>Description description description description description description description description description description description description description description description description</dd>
-                                            <dt>Term term term</dt>
-                                            <dd>Description description description description description description description description description description description description description description description description</dd>
-                                        </dl>
-
-
-                                        <p>Nullam euismod turpis tortor. Vivamus non mi ac nulla iaculis facilisis ac euismod mauris. Mauris ac augue eu quam venenatis scelerisque. Morbi id elementum erat. Duis tempor quam mollis libero rutrum eu sagittis quam porta. Nunc tempus, arcu sed blandit vehicula, massa justo hendrerit magna, ac congue sapien enim quis nibh. Proin at lacus non nisi tempus imperdiet. Sed non erat ultricies eros consectetur pellentesque. Nam cursus rhoncus volutpat. Ut sagittis elit sit amet nisi fermentum vitae varius tellus interdum. Aliquam hendrerit, felis eu pulvinar sagittis, erat mi venenatis dolor, sed mattis felis ante aliquet erat. Mauris luctus massa a nulla pulvinar ornare. Donec dapibus tortor tempus lectus tincidunt viverra.</p>
-                                        <p>Suspendisse aliquam ultricies velit, sed vehicula magna fringilla vitae. Sed odio augue, ultricies fermentum pulvinar ac, tincidunt et purus. Nullam enim arcu, rutrum auctor sagittis vel, blandit id metus. Curabitur dignissim condimentum dolor, a tincidunt purus eleifend in. Sed eleifend pulvinar nisi sit amet fringilla. Proin mattis venenatis vestibulum. Cras laoreet massa a sapien pharetra iaculis. Vestibulum nibh diam, ultrices vitae ultrices nec, laoreet sit amet justo. Donec porttitor facilisis vehicula. Praesent turpis tortor, aliquet eget convallis convallis, vulputate viverra nisl. Curabitur molestie congue velit, vel feugiat sem tincidunt sed. Integer lorem libero, ultricies sed molestie et, pellentesque eu metus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus accumsan semper magna sit amet sollicitudin. Nunc a libero at nisi porta imperdiet.</p>
-                                        <p>Vestibulum sed nisl nisi. Sed fringilla nisl sed ante dignissim ut rhoncus nulla posuere. Ut ante turpis, rutrum at venenatis nec, iaculis at lectus. Donec nibh urna, scelerisque eu molestie quis, blandit sed sapien. Cras interdum aliquet metus, ut vulputate mauris adipiscing id. Vestibulum dui erat, gravida eget adipiscing nec, rhoncus a massa. Curabitur dui nisl, convallis quis accumsan tristique, adipiscing tristique nisi. Vestibulum quis mi lorem, ac fermentum ipsum. </p>
 
                                     </div>
                                 </div>
