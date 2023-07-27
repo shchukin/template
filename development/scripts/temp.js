c44310ebaca82380951aec1160d0b058af08ec5b
Anton Shchukin <a.a.shchukin@gmail.com>
2023-07-27 20:09:17 +0700

    Clean up countdown init

--- development/scripts/temp.js
+++ development/scripts/temp.js
@@ -38,61 +38,61 @@
      * jQuery Countdown init
      */
 
-    var newYear = new Date();
-    newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1);
+    let newYear = new Date(new Date().getFullYear() + 1, 0, 1);
+
     $('#new-year').countdown({
         until: newYear,
         format: 'DHM',
         layout: '<div class="countdown__container">' +
-                    '{d<}' +
-                    '<div class="countdown__section">' +
-                        '<span class="countdown__value">' +
-                            '<span class="countdown__digit">' +
-                                '{d100}' +
-                            '</span>' +
-                            '<span class="countdown__digit">' +
-                                '{d10}' +
-                            '</span>' +
-                            '<span class="countdown__digit">' +
-                                '{d1}' +
-                            '</span>' +
-                        '</span>' +
-                        '<span class="countdown__label">' +
-                            '{dl}' +
-                        '</span>' +
-                    '</div>' +
-                    '{d>}' +
-                    '{h<}' +
-                    '<div class="countdown__section">' +
-                        '<span class="countdown__value">' +
-                            '<span class="countdown__digit">' +
-                                '{h10}' +
-                            '</span>' +
-                            '<span class="countdown__digit">' +
-                                '{h1}' +
-                            '</span>' +
-                        '</span>' +
-                        '<span class="countdown__label">' +
-                            '{hl}' +
-                        '</span>' +
-                    '</div>' +
-                    '{h>}' +
-                    '{m<}' +
-                    '<div class="countdown__section">' +
-                        '<span class="countdown__value">' +
-                            '<span class="countdown__digit">' +
-                                '{m10}' +
-                            '</span>' +
-                            '<span class="countdown__digit">' +
-                                '{m1}' +
-                            '</span>' +
-                        '</span>' +
-                        '<span class="countdown__label">' +
-                            '{ml}' +
-                        '</span>' +
-                    '</div>' +
-                    '{m>}' +
-                '</div>'
+            '{d<}' +
+            '<div class="countdown__section">' +
+                '<span class="countdown__value">' +
+                    '<span class="countdown__digit">' +
+                        '{d100}' +
+                    '</span>' +
+                    '<span class="countdown__digit">' +
+                        '{d10}' +
+                    '</span>' +
+                    '<span class="countdown__digit">' +
+                        '{d1}' +
+                    '</span>' +
+                '</span>' +
+                '<span class="countdown__label">' +
+                    '{dl}' +
+                '</span>' +
+            '</div>' +
+            '{d>}' +
+            '{h<}' +
+            '<div class="countdown__section">' +
+                '<span class="countdown__value">' +
+                    '<span class="countdown__digit">' +
+                        '{h10}' +
+                    '</span>' +
+                    '<span class="countdown__digit">' +
+                        '{h1}' +
+                    '</span>' +
+                '</span>' +
+                '<span class="countdown__label">' +
+                    '{hl}' +
+                '</span>' +
+            '</div>' +
+            '{h>}' +
+            '{m<}' +
+            '<div class="countdown__section">' +
+                '<span class="countdown__value">' +
+                    '<span class="countdown__digit">' +
+                        '{m10}' +
+                    '</span>' +
+                    '<span class="countdown__digit">' +
+                        '{m1}' +
+                    '</span>' +
+                '</span>' +
+                '<span class="countdown__label">' +
+                    '{ml}' +
+                '</span>' +
+            '</div>' +
+            '{m>}' +
+        '</div>'
     });
 
 
