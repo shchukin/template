17e64a993ad22b94f41e5b0fb8e0735b9c226c85
Anton Shchukin <a.a.shchukin@gmail.com>
2023-08-08 13:28:58 +0700

    input js clean up

--- src/scripts/_input.js
+++ src/scripts/_input.js
@@ -8,14 +8,11 @@
         }
     });
 
-
     $('.input--expandable .input__widget').each(function () {
         $(this).css('height', ($(this)[0].scrollHeight + 2 * parseInt($(this).css('border-width'), 10)) + 'px');
     }).on('input', function () {
-        console.log(this.scrollHeight)
         $(this).css('height', 'auto');
         $(this).css('height', ($(this)[0].scrollHeight + 2 * parseInt($(this).css('border-width'), 10)) + 'px');
     });
-
 
 })(jQuery);
