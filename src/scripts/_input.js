e4d40032fc4b3bdc8d348493fa39b47291e86dcc
Anton Shchukin <a.a.shchukin@gmail.com>
2023-08-09 11:35:40 +0700

    simplify input types

--- src/scripts/_input.js
+++ src/scripts/_input.js
@@ -1,6 +1,7 @@
 (function ($) {
 
-    $('.input--select .input__widget').on('change', function () {
+    /* Select placeholder */
+    $('select.input__widget').on('change', function () {
         if ($(this).val() === 'placeholder') {
             $(this).parent('.input').addClass('input--placeholder-is-chosen')
         } else {
@@ -8,6 +9,7 @@
         }
     });
 
+    /* Expanding textarea */
     $('.input--expandable .input__widget').each(function () {
         $(this).css('height', ($(this)[0].scrollHeight + 2 * parseInt($(this).css('border-width'), 10)) + 'px');
     }).on('input', function () {
@@ -15,6 +17,7 @@
         $(this).css('height', ($(this)[0].scrollHeight + 2 * parseInt($(this).css('border-width'), 10)) + 'px');
     });
 
+    /* Error field */
     $('.input__widget').on('focus', function () {
         $(this).parents('.input').removeClass('input--error');
         $(this).parents('.input').nextUntil(':not(.helper--output)').remove();
