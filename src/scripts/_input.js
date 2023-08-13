3e20b046ba515d4a86b0b7f2cc8809dd9973958b
Anton Shchukin <a.a.shchukin@gmail.com>
2023-08-13 11:53:48 +0700

    select placeholder as a function

--- src/scripts/_input.js
+++ src/scripts/_input.js
@@ -1,18 +1,24 @@
 (function ($) {
 
     /* Select placeholder */
-    $('select.input__widget').on('change', function () {
-        if ($(this).val() === 'placeholder') {
-            $(this).parent('.input').addClass('input--placeholder-is-chosen')
+    function selectPlaceholder($element) {
+        if ($element.val() === 'placeholder') {
+            $element.parent('.input').addClass('input--placeholder-is-chosen')
         } else {
-            $(this).parent('.input').removeClass('input--placeholder-is-chosen')
+            $element.parent('.input').removeClass('input--placeholder-is-chosen')
         }
+    }
+
+    $('select.input__widget').each(function () {
+        selectPlaceholder( $(this) );
+    }).on('change', function () {
+        selectPlaceholder( $(this) );
     });
 
     /* Expanding textarea */
-    function expandTextarea(element) {
-        element.css('height', 'auto');
-        element.css('height', (element[0].scrollHeight + 2 * parseInt(element.css('border-width'), 10)) + 'px');
+    function expandTextarea($element) {
+        $element.css('height', 'auto');
+        $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
     }
 
     $('.input--expandable .input__widget').each(function () {
